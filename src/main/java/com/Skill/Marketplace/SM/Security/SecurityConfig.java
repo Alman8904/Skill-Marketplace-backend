6451e2bbb7d1db8f2fae1f9ba3796011b6bac3eb
Alman <215115964+Alman8904@users.noreply.github.com>
2026-02-17 21:41:15 +0530

    add healthcheck endpoint

--- src/main/java/com/Skill/Marketplace/SM/Security/SecurityConfig.java
+++ src/main/java/com/Skill/Marketplace/SM/Security/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
-                                "/v3/api-docs.yaml"
+                                "/v3/api-docs.yaml",
+                                "/"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
